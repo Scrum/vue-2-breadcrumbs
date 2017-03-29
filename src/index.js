add9b00fee87196c83698f5769fe2533b980e5d5
Ivan Demidov <demidov@220-volt.ru>
2017-03-29 11:01:44 +0300

    feat(index): disable last link

--- src/index.js
+++ src/index.js
@@ -9,7 +9,13 @@ export default {
 		});
 
 		Vue.component('breadcrumbs', {
-			template: `<ol class="breadcrumb" v-if="$breadcrumbs.length"><li class="breadcrumb-item" v-if="crumb.meta.breadcrumb" v-for="(crumb, i) in $breadcrumbs"><router-link :to=" { path: crumb.path }">{{ crumb.meta.breadcrumb }}</router-link></li></ol>`
+			template: `
+				<ol class="breadcrumb" v-if="$breadcrumbs.length">
+					<li class="breadcrumb-item" v-if="crumb.meta.breadcrumb" v-for="(crumb, i) in $breadcrumbs">
+						<router-link active-class="active" :to="{ path: crumb.path }" :tag="i != $breadcrumbs.length - 1 ? 'a' : 'span'">{{ crumb.meta.breadcrumb }}</router-link>
+					</li>
+				</ol>
+				`
 		});
 	}
 };
