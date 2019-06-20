a13100f7a2cd4df17a84e10d182e0525a1c30292
Ivan Demidov <Scrum@list.ru>
2019-06-20 18:53:58 +0300

    refactor: small )

---
+++ src/components/breadcrumbs.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    name:"breadcrumbs",
+    methods: {
+        getBreadcrumb(bc) {
+            return typeof bc === 'function' ? bc.call(this, this.$route.params) : bc;
+        }
+    },
+    template: `
+        <ol
+            class="breadcrumb"
+            v-if="$breadcrumbs.length"
+        >
+            <li
+                class="breadcrumb-item"
+                v-if="crumb.meta.breadcrumb"
+                v-for="(crumb, i) in $breadcrumbs"
+                :key="crumb"
+            >
+                <router-link
+                    active-class="active"
+                    :to="{ path: crumb.path }"
+                    :tag="i != $breadcrumbs.length - 1 ? 'a' : 'span'"
+                >
+                    {{ getBreadcrumb(crumb.meta.breadcrumb) }}
+                </router-link>
+            </li>
+        </ol>`
+}
+</script>
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import plugin from './plugin';
+
+export {
+  plugin as default,
+  plugin as VueBreadcrumbsPlugin
+};
