92274e10f3f8dca51bd9845006f7b211512080e6
Ivan Demidov <Scrum@list.ru>
2020-05-08 14:47:06 +0300

    test: fix after update deps

--- test/helpers/setup.js
+++ test/helpers/setup.js
@@ -1,8 +1,19 @@
-require('browser-env')();
+// ./test/_setup.js
+
+// Set up JSDom.
+require('jsdom-global')()
+
+// Fix the Date object, see <https://github.com/vuejs/vue-test-utils/issues/936#issuecomment-415386167>.
+window.Date = Date
+
+// Setup browser environment
 const hooks = require('require-extension-hooks');
 const Vue = require('vue');
 
+// Setup Vue.js to remove production tip
 Vue.config.productionTip = false;
 
+// Setup vue files to be processed by `require-extension-hooks-vue`
 hooks('vue').plugin('vue').push();
-hooks(['vue', 'js']).plugin('babel').push();
+// Setup vue and js files to be processed by `require-extension-hooks-babel`
+hooks(['vue', 'js']).exclude(({filename}) => filename.match(/\/node_modules\//)).plugin('babel').push();
