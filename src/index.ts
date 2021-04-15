bb0a6e831c9cdd3a4214aedc5319c20841e50110
Ivan Demidov <Scrum@list.ru>
2021-04-15 15:56:01 +0300

    feat: resolve by name for nuxtjs, close #115

--- src/index.ts
+++ src/index.ts
@@ -30,6 +30,12 @@ class VueBreadcrumbs implements PluginObject<ComponentOptions<Vue>> {
 
               let breadcrumb = routeParentLast.meta?.breadcrumb;
 
+              if (breadcrumb === undefined) {
+                if (routeParentLast.name) {
+                  breadcrumb = routeParentLast.name;
+                }
+              }
+
               if (typeof breadcrumb === 'function') {
                 breadcrumb = breadcrumb.call(this, this.$route.params);
               }
@@ -41,10 +47,46 @@ class VueBreadcrumbs implements PluginObject<ComponentOptions<Vue>> {
             return routeParents.concat(matches);
           }
 
+          function resolveByName(route: RouteRecord, params: Record<string, any>): string {
+            route.meta.breadcrumb = {};
+
+            const isRoot: boolean = route.parent === undefined;
+            const isRootChildren = isRoot === false && route.path.split('/').reverse()[0].length === 0;
+            let label = route.name;
+
+            if (isRoot && label === undefined) {
+              label = route.path.replace(/\//, '');
+              route.meta.breadcrumb = label;
+            }
+
+            if (isRootChildren) {
+              if (label && route.parent) {
+                route.parent.meta.breadcrumb = label;
+              }
+              delete route.meta.breadcrumb;
+            }
+
+            if (isRoot === false && isRootChildren === false) {
+              label = label?.split('-').reverse()[0];
+
+              if (label && Reflect.has(params, label)) {
+                label = params[label];
+              }
+
+              route.meta.breadcrumb = label;
+            }
+
+            return label ?? ''
+          }
+
           return this.$route.matched
             .flatMap((route: RouteRecord) => {
               let routeRecord: RouteRecord[] = [route];
               let breadcrumb = route.meta?.breadcrumb;
+
+              if (breadcrumb === undefined) {
+                breadcrumb = resolveByName(route, this.$route.params);
+              }
 
               if (typeof breadcrumb === 'function') {
                 breadcrumb = breadcrumb.call(this, this.$route.params);
